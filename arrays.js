994b41afe113c15567ed3bf514629dd55ba25c9e
Jo-Eazy <josedhlamini9@gmail.com>
2025-10-29 14:54:30 +0200

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,14 @@
+let colours = [];
+
+colours.push("emerald");
+colours.push("sapphire");
+colours.push("ruby")
+
+let numbers = [10, 20, 30, 40, 50];
+
+colours.pop();
+
+colours.unshift("amber");
+
+console.log("The length of the colours array is:", colours.length);
+console.log("The length of the numbers array is:", numbers.length);
